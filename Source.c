9bf185d0c51ccc5651be1400bec9dfa51347f058
Morv <morvan02@yahoo.com>
2013-09-12 14:15:18 -0400

    Update Source.c
    cleaned up some of the code

--- Source.c
+++ Source.c
@@ -16,7 +16,7 @@ Purpose: odds program.
 #define pause system("pause")
 #define clr system("cls")
 
-//FUNCTION PROTOTYPES
+//FUNCTION PROTOTYPES 
 int getUserChoice();
 void drawMenu(); //1. place bet 2. select horse 3. account options 4. quit
 int accountOptions(double *balance); //1. Add money 2. Cash out 3.Main menu 4. quit
@@ -31,17 +31,12 @@ void displayResults(int winner);
 
 main() 
 {
-	double balance = 0;
-	int horse = 0, winner = 0, userChoice = 0;
-	int standings[] = {0};
-
+	int userChoice = 0;
+	
 	do{
 		getUserChoice();
 
 	}while (userChoice != 4);
-
-
-
 
 system("pause");
 }//end main
@@ -49,14 +44,16 @@ int getUserChoice(){
 	int userChoice = 0;
 	drawMenu();
 	scanf("%i", &userChoice);
-	clr;
+	//make function to verify user's input matches selection in drawMenu function
 
+	flush;
+	clr;
 	return userChoice;
 }//end get user choice
 void drawMenu(){
 	printf("~~~~~~~~~~~~~~~~~~~~~~~~\n");
 	printf("Welcome to Zack's Track\n");
 	printf("1. Place a bet\n2. Select a horse\n3. Account options\n4. Quit");
-	printf("~~~~~~~~~~~~~~~~~~~~~~~~\n");
+	printf("~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
 	printf("Enter your choice (1-4):");
-}
+}//end drawMenu
