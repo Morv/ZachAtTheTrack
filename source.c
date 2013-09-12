8c6086139449286ec205565e290e12f270af4a46
Morv <morvan02@yahoo.com>
2013-09-12 17:18:16 -0400

    source.c created
    Template for Assignment 4: Zach At The Track

---
+++ source.c
@@ -0,0 +1,23 @@
+/****************************************************
+Written by: Josh, Justin, Kyle, Morvan
+Date Written: September 12, 2013
+Purpose: Assignment 4: Zach At The Track
+
+1. add/remove money from account
+2. select and bet on a horse
+3. run a random race and payout odds
+4. keep track of horses that won and number of wins
+****************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#define pause system ("pause")
+#define cls system ("cls")
+#define flush fflush (stdin)
+
+// FUNCTION PROTOTYPES
+
+main () {
+
+
+} //end of main
